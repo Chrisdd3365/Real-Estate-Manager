cdf8c3f93c650cb8265b96b5206980491e5570fa
Christophe Durand <christophe.mp.dd@gmail.com>
2021-08-18 07:36:14 +0200

    Replace basic inflation by DataBinding inflation
    We are here using DataBinding to inflate our properties list fragment,
    instead of basic inflation.

--- app/src/main/java/com/openclassrooms/realestatemanager/properties_list/PropertiesListFragment.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/properties_list/PropertiesListFragment.kt
@@ -5,19 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.openclassrooms.realestatemanager.R
+import com.openclassrooms.realestatemanager.databinding.FragmentPropertiesListBinding
 
 class PropertiesListFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_properties_list, container, false)
+    // Helper classes
+    private val viewModel = PropertiesListFragmentViewModel()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+
+        // Inflate the layout for this fragment, using DataBinding
+        val binding = DataBindingUtil.inflate<FragmentPropertiesListBinding>(
+            inflater, R.layout.fragment_properties_list, container, true
+        )
+
+        binding.viewModel = viewModel
+
+        return binding.root
     }
 
     companion object {
+
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
---
+++ app/src/main/java/com/openclassrooms/realestatemanager/properties_list/PropertiesListFragmentViewModel.kt
@@ -0,0 +1,6 @@
+package com.openclassrooms.realestatemanager.properties_list
+
+import androidx.lifecycle.ViewModel
+
+class PropertiesListFragmentViewModel : ViewModel() {
+}
