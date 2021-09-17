a2dc4fecc0eb6f8c9fb1f83a47abb391684b57bc
Christophe Durand <christophe.mp.dd@gmail.com>
2021-09-17 08:48:57 +0200

    Show estate first image in the list

--- app/src/main/java/com/openclassrooms/realestatemanager/properties_list/PropertiesListAdapter.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/properties_list/PropertiesListAdapter.kt
@@ -1,11 +1,16 @@
 package com.openclassrooms.realestatemanager.properties_list
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ObservableField
+import androidx.databinding.ObservableInt
 import androidx.recyclerview.widget.RecyclerView
+import com.openclassrooms.realestatemanager.DatabaseManager
 import com.openclassrooms.realestatemanager.R
 import com.openclassrooms.realestatemanager.databinding.PropertiesListItemBinding
 import com.openclassrooms.realestatemanager.model.Estate
@@ -59,10 +64,52 @@ class PropertiesListAdapter(val clicked : (Estate) -> Unit) : RecyclerView.Adapt
     inner class PropertiesListItemViewHolder(private val binding : PropertiesListItemBinding)
         : RecyclerView.ViewHolder(binding.root) {
 
+        private var bitmap : Bitmap? = null
+        private val viewModel = PropertiesListItemViewModel()
+
         fun setData(context: Context?, estate: Estate) {
-            binding.viewModel = PropertiesListItemViewModel()
+            binding.viewModel = viewModel
+
+            viewModel.setLoading()
+
             binding.viewModel?.setData(context, estate)
             binding.itemRoot.setOnClickListener { clicked.invoke(estate) }
+            setPlaceHolder(estate.typeIndex!!)
+
+            if (bitmap == null && context != null && estate.id != null) {
+                DatabaseManager(context).getImagesForEstate(
+                    estate.id!!,
+                    success = {
+                        bitmap = it[0]
+                        setImage(bitmap!!)
+                    },
+                    failure = {
+                        setPlaceHolder(estate.typeIndex!!)
+                    }
+                )
+            } else if (bitmap != null) {
+                setImage(bitmap!!)
+            } else {
+                setPlaceHolder(estate.typeIndex!!)
+            }
+        }
+
+        private fun setImage(bitmap : Bitmap) {
+            binding.imageView.setImageBitmap(bitmap)
+            binding.imageView.scaleType = ImageView.ScaleType.FIT_XY
+            viewModel.setImage()
+        }
+
+        private fun setPlaceHolder(estateType : Int?) {
+            when (estateType) {
+                0 -> { binding.imageView.setImageResource(R.drawable.ic_flat) }
+                1 -> { binding.imageView.setImageResource(R.drawable.ic_townhouse) }
+                2 -> { binding.imageView.setImageResource(R.drawable.ic_penthouse) }
+                3 -> { binding.imageView.setImageResource(R.drawable.ic_house) }
+                4 -> { binding.imageView.setImageResource(R.drawable.ic_duplex) }
+                null -> { binding.imageView.setImageResource(R.drawable.ic_flat) }
+            }
+            viewModel.setImage()
         }
 
     }
@@ -73,6 +120,9 @@ class PropertiesListAdapter(val clicked : (Estate) -> Unit) : RecyclerView.Adapt
         val city = ObservableField("")
         val price = ObservableField("")
 
+        val imageVisibility = ObservableInt(View.GONE)
+        val loadingImageVisibility = ObservableInt(View.VISIBLE)
+
         fun setData(context: Context?, estate: Estate) {
 
             if (estate.typeIndex != null)
@@ -81,6 +131,16 @@ class PropertiesListAdapter(val clicked : (Estate) -> Unit) : RecyclerView.Adapt
 
             city.set(estate.address)
             price.set("${estate.price} $")
+        }
+
+        fun setImage() {
+            loadingImageVisibility.set(View.GONE)
+            imageVisibility.set(View.VISIBLE)
+        }
+
+        fun setLoading() {
+            imageVisibility.set(View.GONE)
+            loadingImageVisibility.set(View.VISIBLE)
         }
 
     }
