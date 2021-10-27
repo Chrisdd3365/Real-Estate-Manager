0360e564d2f5c837054f8c284f849fbcdd5e7065
Christophe Durand <christophe.mp.dd@gmail.com>
2021-10-27 15:20:01 +0200

    Loaning simulator is now working
    - Handling "0" duration (division by 0 will not crash)
    - Handling no bring amount
    - Rounding up to 2 trailing digits
    - Removed trailing 0s
    - Setting plain string results (no scientific notation)

--- app/src/main/java/com/openclassrooms/realestatemanager/loaning_simulator/LoaningSimulatorActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/loaning_simulator/LoaningSimulatorActivity.kt
@@ -4,11 +4,16 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.MenuItem
-import androidx.appcompat.widget.Toolbar
+import android.widget.EditText
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.slider.Slider
 import com.openclassrooms.realestatemanager.R
 import com.openclassrooms.realestatemanager.databinding.ActivityLoaningSimulatorBinding
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class LoaningSimulatorActivity : AppCompatActivity() {
 
@@ -16,6 +21,20 @@ class LoaningSimulatorActivity : AppCompatActivity() {
     private val viewModel = LoaningSimulatorActivityViewModel()
 
     // Layout variables
+    private var mortgageAmountEt : EditText? = null
+    private var bringAmountEt : EditText? = null
+    private var mortgageDurationEt : EditText? = null
+    private var interestRateSlider : Slider? = null
+
+    private val textWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            calculateResults()
+        }
+
+        override fun afterTextChanged(s: Editable?) {}
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,8 +49,71 @@ class LoaningSimulatorActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         // Init layout variables
+        mortgageAmountEt = binding.mortgageAmountEditText
+        bringAmountEt = binding.bringAmountEditText
+        mortgageDurationEt = binding.mortgageDurationEditText
+        interestRateSlider = binding.interestRateSlider
 
-//        setSupportActionBar(toolbar)
+        viewModel.hideResults()
+
+        setupChangesListeners()
+    }
+
+    private fun setupChangesListeners() {
+        mortgageAmountEt?.addTextChangedListener(textWatcher)
+        bringAmountEt?.addTextChangedListener(textWatcher)
+        mortgageDurationEt?.addTextChangedListener(textWatcher)
+
+        interestRateSlider?.addOnChangeListener { _, _, _ ->
+            calculateResults()
+        }
+    }
+
+    /**
+     *  Main loaning calculation.
+     *  Retrieve data in [EditText]s and calculate the mortgage total cost, and monthly payments.
+     */
+    private fun calculateResults() {
+        val mortgageAmountText = mortgageAmountEt?.text?.toString()
+        val bringAmountText = bringAmountEt?.text?.toString()
+        val mortgageDurationText = mortgageDurationEt?.text?.toString()
+        val interestRate = interestRateSlider?.value
+
+        if (mortgageAmountText.isNullOrEmpty() || mortgageDurationText.isNullOrEmpty()
+            || interestRate == null || mortgageDurationText.toInt() == 0)
+                return
+
+        val mortgageAmount = mortgageAmountText.toDouble()
+        val bringAmount = if (bringAmountText.isNullOrEmpty()) 0.0 else bringAmountText.toDouble()
+        val mortgageDuration = mortgageDurationText.toInt()
+
+        val totalLoaned = mortgageAmount.minus(bringAmount)
+        val totalInterests = totalLoaned / 100 * interestRate
+        val monthlyPayments = (totalLoaned + totalInterests) / mortgageDuration
+
+        displayResults(totalInterests, monthlyPayments)
+    }
+
+    /**
+     *  Rounds up the results before displaying it with [viewModel].
+     *  @param totalCost [Double] - Total cost of the mortgage
+     *  @param monthlyPayments [Double] - The amount of monthly payments
+     */
+    private fun displayResults(totalCost : Double, monthlyPayments : Double) {
+
+        // TODO : We might set BigDecimal in a separate function
+        val roundedTotalCost = BigDecimal(totalCost)
+            .setScale(2, RoundingMode.UP)
+            .stripTrailingZeros()
+            .toPlainString()
+        val roundedMonthlyPayments = BigDecimal(monthlyPayments)
+            .setScale(2, RoundingMode.UP)
+            .stripTrailingZeros()
+            .toPlainString()
+
+        viewModel.updateTotalCostValue(roundedTotalCost)
+        viewModel.updateMonthlyPaymentsValue(roundedMonthlyPayments)
+        viewModel.setResults()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
--- app/src/main/java/com/openclassrooms/realestatemanager/loaning_simulator/LoaningSimulatorActivityViewModel.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/loaning_simulator/LoaningSimulatorActivityViewModel.kt
@@ -1,11 +1,15 @@
 package com.openclassrooms.realestatemanager.loaning_simulator
 
 import android.view.View
+import androidx.databinding.ObservableField
 import androidx.databinding.ObservableInt
 
 class LoaningSimulatorActivityViewModel {
 
     val resultsVisibility = ObservableInt(View.GONE)
+
+    val mortgageTotalCostValue = ObservableField("")
+    val mortgageMonthlyPaymentsValue = ObservableField("")
 
     fun setResults() {
         resultsVisibility.set(View.VISIBLE)
@@ -13,6 +17,14 @@ class LoaningSimulatorActivityViewModel {
 
     fun hideResults() {
         resultsVisibility.set(View.GONE)
+    }
+
+    fun updateTotalCostValue(newValue : String) {
+        mortgageTotalCostValue.set(newValue)
+    }
+
+    fun updateMonthlyPaymentsValue(newValue: String) {
+        mortgageMonthlyPaymentsValue.set(newValue)
     }
 
 }
