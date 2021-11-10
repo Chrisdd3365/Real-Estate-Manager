07049308d2a5634ea5ffa7d3bc89afc310ef6905
Christophe Durand <christophe.mp.dd@gmail.com>
2021-11-10 19:02:55 +0100

    Cleaning imports

--- app/src/main/java/com/openclassrooms/realestatemanager/estate_creation/add_images/AddPicturesFragmentViewModel.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/estate_creation/add_images/AddPicturesFragmentViewModel.kt
@@ -1,7 +1,5 @@
 package com.openclassrooms.realestatemanager.estate_creation.add_images
 
-import android.view.View
-import androidx.databinding.ObservableInt
 import androidx.lifecycle.ViewModel
 
 class AddPicturesFragmentViewModel : ViewModel()
--- app/src/main/java/com/openclassrooms/realestatemanager/properties_list/PropertiesListAdapter.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/properties_list/PropertiesListAdapter.kt
@@ -3,7 +3,6 @@ package com.openclassrooms.realestatemanager.properties_list
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Bitmap
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
