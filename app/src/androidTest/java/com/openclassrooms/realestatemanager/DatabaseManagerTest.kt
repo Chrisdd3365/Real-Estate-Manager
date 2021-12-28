0f2fd17a659c0ae56bc7eff8fe03cd4ff878eb8b
Christophe DURAND <cdurand.dev@gmail.com>
2021-12-28 22:59:29 +0100

    implementing tests for databasemanager

--- app/src/androidTest/java/com/openclassrooms/realestatemanager/DatabaseManagerTest.kt
+++ app/src/androidTest/java/com/openclassrooms/realestatemanager/DatabaseManagerTest.kt
@@ -1,16 +1,22 @@
 package com.openclassrooms.realestatemanager
 
+import android.util.Log
+import android.widget.Toast
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import com.openclassrooms.realestatemanager.DatabaseManager.Companion.getBoolean
+import com.openclassrooms.realestatemanager.estate_creation.EstateCreationActivity
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
 import com.openclassrooms.realestatemanager.model.Estate
+import com.openclassrooms.realestatemanager.utils.Enums
 import org.junit.After
 
 import org.junit.Before
+import java.util.*
 
 @RunWith(AndroidJUnit4::class)
 class DatabaseManagerTest {
@@ -23,6 +29,27 @@ class DatabaseManagerTest {
     @Before
     fun setUp() {
         databaseManager = DatabaseManager(InstrumentationRegistry.getInstrumentation().targetContext)
+
+        estate.id = 0
+        estate.typeIndex = 1
+        estate.description = "description"
+        estate.address = "address"
+        estate.onMarketSince = Calendar.getInstance()
+        estate.setPrice(77777777.0)
+        estate.setSurface(77.0)
+        estate.roomCount = 3
+        estate.bathroomsCount = 2
+        estate.bedroomsCount = 2
+        estate.school = true
+        estate.playground = true
+        estate.shop = true
+        estate.buses = true
+        estate.subway = true
+        estate.park = true
+        estate.latitude = 42.0
+        estate.longitude = 2.0
+        estate.sold = true
+        estate.soldDate = Calendar.getInstance()
     }
 
     @After
@@ -37,11 +64,10 @@ class DatabaseManagerTest {
 
     @Test
     fun testShouldSaveEstate() {
-        estate.id = 0
         databaseManager.saveEstate(
             estate,
             onSuccess = { insertedId ->
-                assertEquals(0, insertedId)
+                assertEquals(1, insertedId)
             },
             onFailure = {
                 assertEquals(null, null)
@@ -51,32 +77,107 @@ class DatabaseManagerTest {
 
     @Test
     fun testShouldUpdateEstate() {
-
+        estate.description = "description 2"
+        databaseManager.updateEstate(
+            estate,
+            onSuccess = {
+                assertEquals("description 2", estate.description)
+            },
+            onFailure = {
+                assertEquals(null, null)
+            }
+        )
     }
 
     @Test
     fun testShouldDeleteEstate() {
-
+        databaseManager.deleteEstate(
+            1,
+            onSuccess = {
+                assertEquals(null, estate.id)
+            },
+            onFailure = {
+                assertEquals(null, null)
+            }
+        )
     }
 
     @Test
     fun testShouldGetAllEstates() {
-
-    }
-
-    @Test
-    fun testShouldGetImagesForEstate() {
+        databaseManager.saveEstate(
+            estate,
+            onSuccess = { insertedId ->
+                databaseManager.getEstates(
+                    success = { estatesList ->
+                        assertEquals(estatesList.count(), estatesList.count())
+                    },
+                    failure = {
+                        assertEquals(null, null)
+                    }
+                )
+            },
+            onFailure = {
+                assertEquals(null, null)
+            }
+        )
 
     }
 
     @Test
     fun testShouldSaveEstateImage() {
+        val pictureUri = "pictureUri"
+        databaseManager.saveEstateImage(
+            estate.id!!,
+            pictureUri,
+            onSuccess = {
+                assertEquals("pictureUri", pictureUri)
+            },
+            onFailure = {
+                assertEquals(null, null)
+            }
+        )
+    }
+
+    @Test
+    fun testShouldGetImagesForEstate() {
+        val pictureUri = "pictureUri"
+        databaseManager.saveEstateImage(
+            estate.id!!,
+            pictureUri,
+            onSuccess = {
+                databaseManager.getImagesForEstate(
+                    estate.id!!,
+                    success = { imagesList ->
+                        assertEquals(imagesList.count(), imagesList.count())
+                    },
+                    failure = {
+                        assertEquals(null, null)
+                    }
+                )
+            },
+            onFailure = {
+                assertEquals(null, null)
+            }
+        )
 
     }
 
     @Test
     fun testShouldDeleteImagesForEstate() {
-
+        databaseManager.deleteImagesForEstate(
+            estate.id!!,
+            onSuccess = {
+                databaseManager.getImagesForEstate(
+                    estate.id!!,
+                    success = { imagesList ->
+                        assertEquals(imagesList.count(), imagesList.count())
+                    },
+                    failure = {
+                        assertEquals(null, null)
+                    }
+                )
+            }
+        )
     }
 
     @Test
